a181b899aa43a268857b5e134df3da0dd9992daa
daniel-salgado <daniel@salgadobr.com>
2018-05-31 01:24:10 +1200

    Inserting and updating customer

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -13,9 +13,15 @@ namespace Vidly.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
         public bool IsSubscribedToNewsletter { get; set; }
+
         public MembershipType MembershipType { get; set; }
+
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeID { get; set; }
+
+        [Display(Name = "Date of birth")]
         public DateTime? Birthdate { get; set; }
     }
 }
